64f2b36795a53da8d7b6fd14c2cccb46d8f1b74f
Ali Murad <samejoalimurad321@gmail.com>
2026-07-03 21:08:26 +0500

    Assignment updated

---
+++ app2.js
@@ -0,0 +1,15 @@
+function colorChangeRed(){
+   document.body.style.backgroundColor="red"
+}
+function colorChangeBlue(){
+   document.body.style.backgroundColor="blue"
+}
+function colorChangeGreen(){
+   document.body.style.backgroundColor="green"
+}
+function colorChangePurple(){
+   document.body.style.backgroundColor="purple"
+}
+function colorChangeYellow(){
+   document.body.style.backgroundColor="yellow"
+}
